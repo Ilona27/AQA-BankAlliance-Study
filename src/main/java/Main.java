24bb63634a4c73c15edad264ba9fe60f803435a0
Кохан Ілона Сергіївна <ikokhan@alliancedigital.tech>
2026-02-20 14:09:39 +0200

    lesson 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Practise practise = new Practise();
+        System.out.println(practise.isAmountString(2000));
+    }
+
+    //1. Перевірка позитивного числа
+    // 2. Перевірка теператури (double, if, > 37.2 -> true)
+    // 3. Порахувати площу прямокутника
 }
--- src/main/java/Practise.java
+++ src/main/java/Practise.java
@@ -1,2 +1,22 @@
 public class Practise {
+
+    public boolean isAdult(int age) {
+        return age >= 18;
+    }
+
+    public boolean isAmount(int amount) {
+        if (amount > 1000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String isAmountString(int amount) {
+        if (amount > 1000) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
 }
