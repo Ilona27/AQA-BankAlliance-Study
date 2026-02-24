1a58918eacd1af6d49c27a0952b0072bf3dada60
Кохан Ілона Сергіївна <ikokhan@alliancedigital.tech>
2026-02-24 12:13:54 +0200

    added new method

---
+++ src/main/java/Practice2.java
@@ -0,0 +1,10 @@
+public class Practice2 {
+
+    public String isAmountSecond(int amount) {
+        if (amount > 1000) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+}
