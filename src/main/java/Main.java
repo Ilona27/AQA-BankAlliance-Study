aab86413a8e76aa953ea96b9df8b1328e23e13cf
Nikolai Tretiak <kolya121199@gmail.com>
2026-02-24 08:52:35 +0200

    HomeTask1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         Practise practise = new Practise();
-        System.out.println(practise.isAmountString(2000));
+        System.out.println(practise.isPosNum(2)); // к 1 заданию
+        System.out.println(practise.isPosTemp(36.6)); // к 2 заданию
+        System.out.println(practise.rectangleArea(12,5)); // к 3 заданию
     }
+
 
     //1. Перевірка позитивного числа
     // 2. Перевірка теператури (double, if, > 37.2 -> true)
--- src/main/java/Practise.java
+++ src/main/java/Practise.java
@@ -19,4 +19,20 @@ public class Practise {
             return "false";
         }
     }
+//задание 1
+    public boolean isPosNum (int num) { return num > 0;}
+//задание 2
+    public String isPosTemp (double temp) {
+        if (temp > 37.2) {
+            return "true";
+        }
+        else {
+            return "false";
+        }
+    }
+//задание 3
+    public int rectangleArea(int a, int b) {
+        return a * b;
+    }
 }
+
