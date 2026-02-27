d32482bae472305d969d01df9943420763580f1b
Nikolai Tretiak <kolya121199@gmail.com>
2026-02-27 13:29:46 +0200

    HomeTask1 with correct packages

--- src/main/java/homework/lesson2/NT/Main.java
+++ src/main/java/homework/lesson2/NT/Main.java
@@ -1,7 +1,8 @@
-public class Main {
+package homework.lesson2.NT;
 
+public class Main {
     public static void main(String[] args) {
-        Practise practise = new Practise();
+        HomeWorkLesson2Tretiak practise = new HomeWorkLesson2Tretiak();
         System.out.println(practise.isPosNum(2)); // к 1 заданию
         System.out.println(practise.isPosTemp(36.6)); // к 2 заданию
         System.out.println(practise.rectangleArea(12,5)); // к 3 заданию
@@ -12,3 +13,5 @@ public class Main {
     // 2. Перевірка теператури (double, if, > 37.2 -> true)
     // 3. Порахувати площу прямокутника
 }
+
+
